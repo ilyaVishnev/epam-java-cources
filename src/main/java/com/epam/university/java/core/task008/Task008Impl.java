901b39023a5b0eab5a05572f6164022a271a3352
ilyaVishnev <mailto:dong85@mail.ru>
2020-08-25 02:53:55 +0300

    eight

---
+++ src/main/java/com/epam/university/java/core/task008/Task008Impl.java
@@ -0,0 +1,59 @@
+package com.epam.university.java.core.task008;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task008Impl implements Task008 {
+
+    /**
+     * Given a string with mathematical expression like "(1 + 2) * {[-3] - 4}". You need to check,
+     * are all braces correct. Ex:
+     * <p>
+     * "(1 + 2) * {[-3] - 4}" - correct
+     * "(1 + [2) + 3 - 4]" - incorrect
+     * In expression can be used the following kinds of braces: {}, (), [].
+     * Tip: it's better to implement stack structure.
+     * Tip: You also can use Stack class but it's not recommended.
+     * </p>
+     *
+     * @param sourceString string to check
+     * @return is braces valid or source string is empty
+     */
+    @Override
+    public boolean isValid(String sourceString) {
+        char[] chars = sourceString.toCharArray();
+        List<Character> braces = new ArrayList<>(Arrays.asList(' '));
+        for (char c : chars) {
+            switch (c) {
+                case '{':
+                    braces.add(c);
+                    break;
+                case '(':
+                    braces.add(c);
+                    break;
+                case '[':
+                    braces.add(c);
+                    break;
+                case '}':
+                    if (braces.remove(braces.size() - 1) != '{') {
+                        return false;
+                    }
+                    break;
+                case ')':
+                    if (braces.remove(braces.size() - 1) != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (braces.remove(braces.size() - 1) != '[') {
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return braces.size() == 1;
+    }
+}

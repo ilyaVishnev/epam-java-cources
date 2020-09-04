304c66dddf8e24a4b57387f027891707e19225e3
ilyaVishnev <mailto:dong85@mail.ru>
2020-09-04 08:29:40 +0300

    17

---
+++ src/main/java/com/epam/university/java/core/task017/Task017Impl.java
@@ -0,0 +1,54 @@
+package com.epam.university.java.core.task017;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class Task017Impl implements Task017 {
+
+    /**
+     * Output objects as formatted string.
+     *
+     * @param args objects to output
+     * @return formatted string
+     */
+    public String formatString(Object... args) {
+        return String.format("You know %s, %s!", args[0], args[1]);
+    }
+
+    /**
+     * Output objects as formatted string.
+     *
+     * @param args objects to output
+     * @return formatted string
+     */
+    public String formatNumbers(Object... args) {
+        String format = "";
+        String str = "";
+        if (args[0] instanceof Double) {
+            format = "f";
+            double dS = (double) args[0];
+            str = Double.toHexString(dS);
+        } else if (args[0] instanceof Long) {
+            format = "d";
+            long lS = (long) args[0];
+            str = Long.toHexString(lS);
+        } else {
+            format = "d";
+            int li = (int) args[0];
+            str = Integer.toHexString(li);
+        }
+        return String.format(Locale.ROOT, "%.1" + format + ", " + "%.2" + format + ", "
+                + "%+.2" + format + ", " + "%s", args[0], args[0], args[0], str);
+    }
+
+    /**
+     * Output objects as formatted string.
+     *
+     * @param args objects to output
+     * @return formatted string
+     */
+    public String formatDates(Object... args) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.dd.MM");
+        return dateFormat.format(args[0]);
+    }
+}

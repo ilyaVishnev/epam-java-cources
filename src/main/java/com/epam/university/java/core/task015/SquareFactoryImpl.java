03b9c412d8e4d4f8a09c8cbb9dfd91a36d85a025
ilyaVishnev <mailto:dong85@mail.ru>
2020-09-30 17:09:21 +0300

    15Changes

--- src/main/java/com/epam/university/java/core/task015/SquareFactoryImpl.java
+++ src/main/java/com/epam/university/java/core/task015/SquareFactoryImpl.java
@@ -4,6 +4,9 @@ public class SquareFactoryImpl implements SquareFactory {
 
     @Override
     public Square newInstance(Point first, Point second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException();
+        }
         Square square = new SquareImpl();
         square.setFirst(first);
         square.setSecond(second);

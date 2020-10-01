20f1fadb0b8a167f93381e94be7776518dd5e0ad
ilyaVishnev <mailto:dong85@mail.ru>
2020-10-01 03:37:39 +0300

    23Changes

--- src/main/java/com/epam/university/java/core/task023/Task023Impl.java
+++ src/main/java/com/epam/university/java/core/task023/Task023Impl.java
@@ -6,12 +6,12 @@ import java.util.regex.Pattern;
 public class Task023Impl implements Task023 {
     @Override
     public String extract(String phoneString) {
-        if (!phoneString.matches("(\\+?)\\d(\\s?)(\\(?)(\\s?)\\d{3}"
-                + "(\\s?)(\\)?)(\\s?)\\d{3}(\\s?)-?\\d{2}(\\s?)-?\\d{2}")
+        if (phoneString == null || !phoneString.matches("(\\+?)\\d(\\s?)-?(\\(?)(\\s?)\\d{3}"
+                + "(\\s?)(\\)?)-?(\\s?)\\d{3}(\\s?)-?\\d{2}(\\s?)-?\\d{2}")
         ) {
             throw new IllegalArgumentException();
         }
-        Pattern pattern = Pattern.compile("(\\+?)\\d(\\s?)(\\(?)(\\s?)\\d{3}");
+        Pattern pattern = Pattern.compile("(\\+?)\\d(\\s?)-?(\\(?)(\\s?)\\d{3}");
         Matcher m = pattern.matcher(phoneString);
         m.find();
         String res = phoneString.substring(m.end() - 3, m.end());

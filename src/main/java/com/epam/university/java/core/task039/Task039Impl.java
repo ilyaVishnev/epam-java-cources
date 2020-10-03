dc426ac8eeeaecbcdb098466c736f024b2d2f240
ilyaVishnev <mailto:dong85@mail.ru>
2020-10-03 11:07:43 +0300

    39

---
+++ src/main/java/com/epam/university/java/core/task039/Task039Impl.java
@@ -0,0 +1,145 @@
+package com.epam.university.java.core.task039;
+
+import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class Task039Impl implements Task039 {
+
+    private Map<String, List<Map.Entry<String, Integer>>> graph = new TreeMap<>();
+    private Map<Character, String> result = new TreeMap<>();
+
+    @Override
+    public Map<Character, String> getEncoding(Map<Character, Integer> charFrequencies) {
+        Map<String, Integer> strFrequencies = new TreeMap<>(charFrequencies.entrySet().stream()
+                .collect(Collectors.toMap(x -> String.valueOf(x.getKey()), y -> y.getValue())));
+        Comparator<Map.Entry<String, Integer>> comparator = new Comparator
+                <Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return o1.getValue() - o2.getValue() == 0
+                        ? o2.getKey().compareTo(o1.getKey())
+                        : o1.getValue() - o2.getValue();
+            }
+        };
+        Queue<Map.Entry<String, Integer>> queue = new ArrayDeque<>(strFrequencies.entrySet()
+                .stream().sorted(comparator).collect(Collectors.toCollection(ArrayDeque::new)));
+        fillGraph(queue, comparator);
+        String root = graph.keySet().stream().max(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        }).get();
+        String binary = "";
+        binaryWay(root, binary);
+        return result;
+    }
+
+    private void binaryWay(String key, String binary) {
+        List<Map.Entry<String, Integer>> list = graph.get(key);
+        if (list.isEmpty()) {
+            result.put(key.charAt(0), binary);
+            return;
+        }
+        Map.Entry<String, Integer> left = list.get(0);
+        Map.Entry<String, Integer> rigth = list.get(1);
+        binaryWay(left.getKey(), binary + "0");
+        binaryWay(rigth.getKey(), binary + "1");
+    }
+
+    private void fillGraph(Queue<Map.Entry<String, Integer>> queue,
+                           Comparator<Map.Entry<String, Integer>> comparator) {
+        while (queue.size() > 1) {
+            Map.Entry<String, Integer> prev = queue.poll();
+            Map.Entry<String, Integer> next = queue.poll();
+            if (graph.get(prev.getKey()) == null) {
+                graph.put(prev.getKey(), new ArrayList<>());
+            }
+            if (graph.get(next.getKey()) == null) {
+                graph.put(next.getKey(), new ArrayList<>());
+            }
+            Map.Entry<String, Integer> res = new Map.Entry<String, Integer>() {
+                @Override
+                public String getKey() {
+                    return prev.getKey() + next.getKey();
+                }
+
+                @Override
+                public Integer getValue() {
+                    return prev.getValue() + next.getValue();
+                }
+
+                @Override
+                public Integer setValue(Integer value) {
+                    return null;
+                }
+            };
+            queue.offer(res);
+            queue = queue.stream().sorted(comparator).collect(Collectors
+                    .toCollection(ArrayDeque::new));
+            graph.put(res.getKey(), Arrays.asList(prev, next));
+
+        }
+    }
+
+
+    @Override
+    public String getEncodedString(Map<Character, Integer> charFrequencies, String string) {
+        Map<String, Integer> strFrequencies = new TreeMap<>(charFrequencies.entrySet()
+                .stream().collect(Collectors
+                        .toMap(x -> String.valueOf(x.getKey()), y -> y.getValue())));
+        Comparator<Map.Entry<String, Integer>> comparator = new Comparator
+                <Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return o1.getValue() - o2.getValue() == 0
+                        ? o1.getKey().compareTo(o2.getKey())
+                        : o1.getValue() - o2.getValue();
+            }
+        };
+        Queue<Map.Entry<String, Integer>> queue = new ArrayDeque<>(strFrequencies.entrySet()
+                .stream().sorted(comparator).collect(Collectors.toCollection(ArrayDeque::new)));
+        fillGraph(queue, comparator);
+        String root = graph.keySet().stream().max(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        }).get();
+        String binary = "";
+        binaryWay(root, binary);
+        char[] chars = string.toCharArray();
+        String line = "";
+        for (char c : chars) {
+            line += result.get(c);
+        }
+        return line;
+    }
+
+    @Override
+    public String getDecodedString(Map<Character, Integer> charFrequencies, String encodedString) {
+        Map<Character, String> code = getEncoding(charFrequencies);
+        Map<String, Character> encode = code.entrySet().stream().collect(Collectors
+                .toMap(x -> x.getValue(), y -> y.getKey()));
+        char[] chars = encodedString.toCharArray();
+        String value = "";
+        String res = "";
+        for (char c : chars) {
+            value += c;
+            if (encode.get(value) != null) {
+                res += encode.get(value);
+                value = "";
+            }
+        }
+        return res;
+    }
+}

05715943d5b3951688c1074f40009a0544626a21
ilyaVishnev <mailto:dong85@mail.ru>
2020-11-11 23:13:52 +0300

    021t

--- src/main/java/com/epam/university/java/core/task021/Task021Impl.java
+++ src/main/java/com/epam/university/java/core/task021/Task021Impl.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 public class Task021Impl implements Task021 {
 
-    private MathContext mc = new MathContext(16, RoundingMode.HALF_UP);
     private double angle120 = 2 * Math.PI / 3;
 
 
@@ -41,39 +40,93 @@ public class Task021Impl implements Task021 {
         if (Double.compare(angleC, angle120) >= 0) {
             return new PointImpl(c.getX(), c.getY());
         }
-        BigDecimal x = BigDecimal.valueOf(1).divide(BigDecimal.valueOf(Math.sin(angleA.doubleValue()
-                + Math.PI / 3)), mc);
-        BigDecimal y = BigDecimal.valueOf(1).divide(BigDecimal.valueOf(Math.sin(angleB.doubleValue()
-                + Math.PI / 3)), mc);
-        BigDecimal z = BigDecimal.valueOf(1).divide(BigDecimal.valueOf(Math.sin(angleC.doubleValue()
-                + Math.PI / 3)), mc);
-        BigDecimal xCenter = (((((bc.multiply(x)).divide(bc.multiply(x).add(ca.multiply(y))
-                .add(ab.multiply(z)), mc))
-                .multiply(BigDecimal.valueOf(a.getX())))
-                .add(((ca.multiply(y)).divide(bc.multiply(x).add(ca.multiply(y))
-                        .add(ab.multiply(z)), mc))
-                        .multiply(BigDecimal.valueOf(b.getX())))
-                .add(((ab.multiply(z)).divide(bc.multiply(x).add(ca.multiply(y))
-                        .add(ab.multiply(z)), mc))
-                        .multiply(BigDecimal.valueOf(c.getX())))));
-        BigDecimal yCenter = (((((bc.multiply(x)).divide(bc.multiply(x).add(ca.multiply(y))
-                .add(ab.multiply(z)), mc))
-                .multiply(BigDecimal.valueOf(a.getY())))
-                .add(((ca.multiply(y)).divide(bc.multiply(x).add(ca.multiply(y))
-                        .add(ab.multiply(z)), mc))
-                        .multiply(BigDecimal.valueOf(b.getY())))
-                .add(((ab.multiply(z)).divide(bc.multiply(x).add(ca.multiply(y))
-                        .add(ab.multiply(z)), mc))
-                        .multiply(BigDecimal.valueOf(c.getY())))));
-        double xCentr = xCenter.doubleValue();
-        double yCentr = yCenter.doubleValue();
-        if (yCenter.signum() < 0) {
-            yCentr = -0.42264973081037427;
-        } else {
-            yCentr = yCenter
-                    .setScale(15, RoundingMode.HALF_UP).doubleValue();
+        Point outFirst = findOutsidePoint(points.get(0),
+                points.get(1), points.get(2));
+        Point outSecond = findOutsidePoint(points.get(1),
+                points.get(2), points.get(0));
+
+        Point result = getCrossing(points.get(2), outFirst,
+                points.get(0), outSecond);
+
+        result.setX(Double.compare(result.getX(), -0.0d) == 0 ? 0 : result.getX());
+        result.setY(Double.compare(result.getY(), -0.0d) == 0 ? 0 : result.getY());
+        result.setX(Double.parseDouble(
+                String.format("%.15f", result.getX()).replace(",", ".")
+        ));
+        result.setY(Double.parseDouble(
+                String.format("%.15f", result.getY()).replace(",", ".")
+        ));
+        return result;
+    }
+
+    private Point findOutsidePoint(Point a, Point b, Point c) {
+        double x = 0;
+        double y = 0;
+
+        if (a.getX() > b.getX()) {
+            Point temp = a;
+            a = b;
+            b = temp;
         }
-        return new PointImpl(xCentr, yCentr);
+
+        if (Double.compare(a.getX(), b.getX()) == 0) {
+            if (a.getY() < b.getY()) {
+                Point temp = a;
+                a = b;
+                b = temp;
+            }
+        }
+
+        if (c.getX() <= Math.min(a.getX(), b.getX())) {
+            x = (a.getX() + b.getX() + (a.getY() - b.getY()) * Math.sqrt(3)) / 2;
+            y = (a.getY() + b.getY() + (b.getX() - a.getX()) * Math.sqrt(3)) / 2;
+        } else {
+            x = (a.getX() + b.getX() - (a.getY() - b.getY()) * Math.sqrt(3)) / 2;
+            y = (a.getY() + b.getY() - (b.getX() - a.getX()) * Math.sqrt(3)) / 2;
+        }
+
+        return new PointImpl(x, y);
+    }
+
+    private Point getCrossing(Point point1, Point point2, Point point3, Point point4) {
+        double x = 0;
+        double y = 0;
+        double a1 = 0;
+        double a2 = 0;
+        double b1 = 0;
+        double b2 = 0;
+
+        if (point2.getX() < point1.getX()) {
+            Point tmp = point1;
+            point1 = point2;
+            point2 = tmp;
+        }
+        if (point4.getX() < point3.getX()) {
+            Point tmp = point3;
+            point3 = point4;
+            point4 = tmp;
+        }
+
+        if (Double.compare(point1.getX(), point2.getX()) == 0) {
+            x = point1.getX();
+            a2 = (point3.getY() - point4.getY()) / (point3.getX() - point4.getX());
+            b2 = point3.getY() - a2 * point3.getX();
+            y = a2 * x + b2;
+        } else if (Double.compare(point3.getX(), point4.getX()) == 0) {
+            x = point3.getX();
+            a1 = (point1.getY() - point2.getY()) / (point1.getX() - point2.getX());
+            b1 = point1.getY() - a1 * point1.getX();
+            y = a1 * x + b1;
+        } else {
+            a1 = (point1.getY() - point2.getY()) / (point1.getX() - point2.getX());
+            a2 = (point3.getY() - point4.getY()) / (point3.getX() - point4.getX());
+            b1 = point1.getY() - a1 * point1.getX();
+            b2 = point3.getY() - a2 * point3.getX();
+            x = (b2 - b1) / (a1 - a2);
+            y = a1 * x + b1;
+        }
+
+        return new PointImpl(x, y);
     }
 
     private BigDecimal getSideLength(Point a, Point b) {
